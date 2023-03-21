17f3b90ff47f4c82bc94f5e1d4b0ea04a3d8bf2f
Maillard <louis.maillard@inist.fr>
2023-03-21 12:03:19 +0100

    Maj affichage médias iiif : helper et vue

--- helper/SortMedia.php
+++ helper/SortMedia.php
@@ -8,8 +8,7 @@ class SortMedia extends AbstractHelper
     public function __invoke($files = null) 
     { 
         $sortedMedia = [];
-        $whitelist = ['image/bmp', 'image/gif', 'image/jpeg', 'image/png', 'image/svg+xml', 'video/flv', 'video/x-flv', 'video/mp4', 'video/m4v',
-                    'video/webm', 'video/wmv', 'video/quicktime', 'application/pdf'];
+        $whitelist = ['image/bmp', 'image/gif', 'image/jpeg', 'image/png', 'image/svg+xml', 'video/flv', 'video/x-flv', 'video/mp4', 'video/m4v', 'video/webm', 'video/wmv', 'video/quicktime', 'application/pdf'];
         $html5videos = [];
         $mediaCount = 0;
 
@@ -23,7 +22,9 @@ class SortMedia extends AbstractHelper
                     $sortedMedia['lightMedia'][$mediaCount]['tracks'] = [];
                 }
                 $mediaCount++;
-            } else {
+            }else if($mediaRenderer == "iiif"){
+                $sortedMedia['iiifMedia'][] = $media;
+            }else{
                 $sortedMedia['otherMedia'][] = $media;
             }
         }
